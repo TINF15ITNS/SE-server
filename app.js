aa2398fe6d8b28ccaef368c479984bad805de22d
kanadagermane <daniel@laube.online>
2017-04-04 16:10:10 +0200

    Abszinentz

---
+++ app.js
@@ -0,0 +1,25 @@
+var PROTO_PATH = __dirname + '/protos/server.proto';
+
+var grpc = require('grpc');
+var hello_proto = grpc.load(PROTO_PATH).serverPackage;
+
+function sayHello(call, callback) {
+  callback(null, {message: 'Hallo ' + call.request.name});
+}
+
+function login(call, callback) {
+  if(call.request.user == 'daniel' && call.request.passwd == '12345') {
+	  callback(null, {message: 'Login successfull'}); 
+  } else {
+	  callback(null, {message: 'Login failed'});
+  }
+}
+
+function main() {
+  var server = new grpc.Server();
+  server.addProtoService(hello_proto.ServerService.service, {sayHello: sayHello, login:login});
+  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+  server.start();
+}
+
+main();
