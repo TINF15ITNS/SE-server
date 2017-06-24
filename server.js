40072859d958251ea2c7176b7f5eb1da8e4b0c8d
Fabian Hinz <hallo@naibafo.de>
2017-06-24 17:47:52 +0200

    remove getUserDetails call

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ server.addProtoService(apiProto.ServerService.service, {
   updatePassword: updatePassword,
   deleteUser: deleteUser,
   searchUser: searchUser,
-  getUserDetails: getUserDetails
 })
 server.bind(config.grpc.uri, grpc.ServerCredentials.createInsecure())
 server.start()
